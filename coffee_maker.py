44cd90457765bcc0672fd37ecbb74819aa0b0acb
brunodos-santos <bruno6.santos@usp.br>
2023-07-21 11:36:20 -0300

    Criação da classe CoffeeMaker

---
+++ coffee_maker.py
@@ -0,0 +1,29 @@
+class CoffeeMaker:
+    """
+    Models the machine that makes the coffee
+    """
+    def __init__(self):
+        self.resources = {
+            "water": 300,
+            "milk": 200,
+            "coffee": 100,
+        }
+
+    def report(self):
+        """
+        :return: Prints a report of all resources.
+        """
+        print(f"Water: {self.resources['water']}ml")
+        print(f"Milk: {self.resources['milk']}ml")
+        print(f"Coffee: {self.resources['coffee']}g")
+
+    def is_resource_sufficient(self, drink):
+        """
+        :return: Returns True when order can be made, False if ingredients are insufficient.
+        """
+        can_make = True
+        for item in drink.ingredients:
+            if drink.ingredients[item] > self.resources[item]:
+                print(f"Sorry there is not enough {item}.")
+                can_make = False
+        return can_make
