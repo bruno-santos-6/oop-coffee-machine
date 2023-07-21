bc798f54fe23fb4d124cbc90c47ef2923202bc4b
brunodos-santos <bruno6.santos@usp.br>
2023-07-21 11:53:20 -0300

    Criação da classe MenuItem

---
+++ menu.py
@@ -0,0 +1,13 @@
+class MenuItem:
+    """
+    Models each Menu Item.
+    """
+    def __int__(self, name, water, milk, coffee, cost):
+        self.name = name
+        self.cost = cost
+        self.ingredients = {
+            "water": water,
+            "milk": milk,
+            "coffee": coffee
+        }
+
